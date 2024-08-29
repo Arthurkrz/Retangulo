562bc7161f71e6e7926e9312ff623865519f3f5f
Arthur K <arthurk031@outlook.com>
2024-08-29 16:26:38 -0300

    Início

---
+++ Calculos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Retangulo
+{
+    public class Calculos
+    {
+        public int largura;
+        public int altura;
+        public double CalcularArea(int largura, int altura)
+        {
+            return largura * altura;
+        }
+        public double CalcularPerimetro(int largura, int altura)
+        {
+            return (largura * 2) + (altura * 2);
+        }
+        public double CalcularDiagonal(int largura, int altura)
+        {
+            return Math.Sqrt((largura * largura) + (altura * altura));
+        }
+        public Calculos(int l, int a)
+        {
+            l = largura;
+            a = altura;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao calculador de retângulo!\n\nInsira, linha por linha, os valores da largura e altura:");
+            int largura = int.Parse(Console.ReadLine());
+            int altura = int.Parse(Console.ReadLine());
+            Calculos r = new Calculos(largura, altura);
+            Console.Clear();
+            string diagonal = r.CalcularDiagonal(largura, altura).ToString("F2");
+            Console.WriteLine($"Área - {r.CalcularArea(largura, altura)};\nPerímetro - {r.CalcularPerimetro(largura, altura)};\nDiagonal - {diagonal}.");
+        }
+    }
+}
