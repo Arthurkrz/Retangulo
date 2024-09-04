fec2c613f4e6f1a25f2700f0facdf8d1303f2624
Arthur K <arthurk031@outlook.com>
2024-09-03 21:24:54 -0300

    Correções

--- Calculos.cs
+++ Calculos.cs
@@ -4,26 +4,26 @@ using System.Text;
 
 namespace Retangulo
 {
-    public class Calculos
+    public class Retangulo
     {
         public int largura;
         public int altura;
-        public double CalcularArea(int largura, int altura)
+        public Retangulo(int larguraRetangulo, int alturaRetangulo)
+        {
+            largura = larguraRetangulo;
+            altura = alturaRetangulo;
+        }
+        public double CalcularArea()
         {
             return largura * altura;
         }
-        public double CalcularPerimetro(int largura, int altura)
+        public double CalcularPerimetro()
         {
             return (largura * 2) + (altura * 2);
         }
-        public double CalcularDiagonal(int largura, int altura)
+        public double CalcularDiagonal()
         {
             return Math.Sqrt((largura * largura) + (altura * altura));
-        }
-        public Calculos(int l, int a)
-        {
-            l = largura;
-            a = altura;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace Retangulo
         static void Main(string[] args)
         {
             Console.WriteLine("Bem vindo ao calculador de retângulo!\n\nInsira, linha por linha, os valores da largura e altura:");
-            int largura = int.Parse(Console.ReadLine());
-            int altura = int.Parse(Console.ReadLine());
-            Calculos r = new Calculos(largura, altura);
+            int larguraRetangulo = int.Parse(Console.ReadLine());
+            int alturaRetangulo = int.Parse(Console.ReadLine());
+            Retangulo r = new Retangulo(larguraRetangulo, alturaRetangulo);
             Console.Clear();
-            string diagonal = r.CalcularDiagonal(largura, altura).ToString("F2");
-            Console.WriteLine($"Área - {r.CalcularArea(largura, altura)};\nPerímetro - {r.CalcularPerimetro(largura, altura)};\nDiagonal - {diagonal}.");
+            string diagonal = r.CalcularDiagonal().ToString("F2");
+            Console.WriteLine($"Área - {r.CalcularArea()};\nPerímetro - {r.CalcularPerimetro()};\nDiagonal - {diagonal}.");
         }
     }
 }
